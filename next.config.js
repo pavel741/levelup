3c2d7896920d251818d6ab8e72a04850e2a4e304
pavel741 <pavel.timerhanov@playtech.com>
2025-12-21 17:13:31 +0200

    Fixed type issues

--- next.config.js
+++ next.config.js
@@ -4,7 +4,24 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  webpack: (config, { isServer }) => {
+    // Exclude MongoDB and Node.js modules from client-side bundle
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        child_process: false,
+        'mongodb-client-encryption': false,
+      }
+      
+      // Exclude MongoDB from client bundle
+      config.externals = config.externals || []
+      config.externals.push('mongodb')
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
-
